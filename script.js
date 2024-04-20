2a9939a44f82847fafcda8ae87b7a8bc1ff96b93
kushal jirel <kushaljirel12@gmail.com>
2024-04-20 23:39:12 +0545

    getting ready for deployment

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// document.querySelector("body").setAttribute("class","bgChange")
+document.querySelector(".blastOn").onclick = function() {
+    document.querySelector('body').classList.add("bgChange")
+    document.querySelector('.purpleimg').classList.add("purple","fireBall")
+    document.querySelector('.lightning').classList.add("lightningBlast")
+}
+
+
+document.querySelector(".blastOff").onclick = function() {
+    document.querySelector('body').classList.remove("bgChange")
+    document.querySelector('.purpleimg').classList.remove("purple","fireBall")
+    document.querySelector('.lightning').classList.remove("lightningBlast")
+    // document.querySelector('.gojoimg').classList.remove("gojo")
+}
